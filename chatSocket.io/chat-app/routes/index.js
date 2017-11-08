40021ae8e1f7a9eadbdda27f4d27622f32a1c5a4
DiegoaMedina <https://diegoamedina@github.com>
2017-11-08 07:54:07 +0000

    added variable to res.render{} for the subtitle

--- chatSocket.io/chat-app/routes/index.js
+++ chatSocket.io/chat-app/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Chat Application' });
+  res.render('index', { title: 'SoundBox', subTitle: 'Chat Application' });
 });
 
 module.exports = router;
