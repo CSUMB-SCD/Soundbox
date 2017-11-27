5ca5af31da1657e8610c4076f7fa5f4c42cbebdf
Mario Martinez <mariomartinez1@csumb.edu>
2017-11-27 08:49:57 +0000

    Downloading only audio

--- index.php
+++ index.php
@@ -1,11 +1,40 @@
 <?php
 
 
-    echo "Hello World";
+    require __DIR__ . '/Youtube-DL/vendor/autoload.php';
+
+    use YoutubeDl\YoutubeDl;
     
-    $output = shell_exec('youtube-dl -x --audio-format mp3 https://www.youtube.com/watch?v=8QxtIQ1F1ig');
-    echo "<pre>$output</pre>";
-    echo getcwd();
-    echo "<h1>Finished</h1>";
+    
+    $dl = new YoutubeDl([
+        'extract-audio' => true,
+        'audio-format' => 'mp3',
+        'audio-quality' => 0, // best
+        'output' => '%(title)s.%(ext)s',
+    ]);
+    
+    $dl->setDownloadPath(__DIR__ . '/Youtube-DL/Audio');
+    
+
+    
+    $video = $dl->download('https://www.youtube.com/watch?v=8QxtIQ1F1ig');
+    
+    echo $video->getTitle() . " <br>";
+    
+    
+    
+    
+    
+    
+    
+    
+    // echo "Hello World";
+    
+    // $output = shell_exec('youtube-dl -x --audio-format mp3 https://www.youtube.com/watch?v=8QxtIQ1F1ig');
+    // echo "<pre>$output</pre>";
+    // echo getcwd();
+    // echo "<h1>Finished</h1>";
+    
+    
 
 ?>
