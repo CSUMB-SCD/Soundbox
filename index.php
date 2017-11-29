0bf89cfa15c7be4c097a79f14022bbfb98c9d557
Mario Martinez <mariomartinez1@csumb.edu>
2017-11-29 02:48:19 +0000

    More testing on downloading videos

--- index.php
+++ index.php
@@ -1,39 +1,39 @@
 <?php
 
 
-    require __DIR__ . '/Youtube-DL/vendor/autoload.php';
+    // require __DIR__ . '/Youtube-DL/vendor/autoload.php';
 
-    use YoutubeDl\YoutubeDl;
+    // use YoutubeDl\YoutubeDl;
     
     
-    $dl = new YoutubeDl([
-        'extract-audio' => true,
-        'audio-format' => 'mp3',
-        'audio-quality' => 0, // best
-        'output' => '%(title)s.%(ext)s',
-    ]);
+    // $dl = new YoutubeDl([
+    //     'extract-audio' => true,
+    //     'audio-format' => 'mp3',
+    //     'audio-quality' => 0, // best
+    //     'output' => '%(title)s.%(ext)s',
+    // ]);
     
-    $dl->setDownloadPath(__DIR__ . '/Youtube-DL/Audio');
+    // $dl->setDownloadPath(__DIR__ . '/Youtube-DL/Audio');
     
 
     
-    $video = $dl->download('https://www.youtube.com/watch?v=8QxtIQ1F1ig');
+    // $video = $dl->download('https://www.youtube.com/watch?v=8QxtIQ1F1ig');
     
-    echo $video->getTitle() . " <br>";
-    
-    
+    // echo $video->getTitle() . " <br>";
     
     
     
     
     
     
-    // echo "Hello World";
     
-    // $output = shell_exec('youtube-dl -x --audio-format mp3 https://www.youtube.com/watch?v=8QxtIQ1F1ig');
-    // echo "<pre>$output</pre>";
-    // echo getcwd();
-    // echo "<h1>Finished</h1>";
+    
+    echo "Hello World";
+    
+    $output = shell_exec('youtube-dl -x --audio-format mp3 https://www.youtube.com/watch?v=8QxtIQ1F1ig');
+    echo "<pre>$output</pre>";
+    echo getcwd();
+    echo "<h1>Finished</h1>";
     
     
 
