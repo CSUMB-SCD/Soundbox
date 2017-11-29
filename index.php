e762ad57b6cab28773bea86b8bbccfada76b1366
Mario Martinez <mariomartinez1@csumb.edu>
2017-11-29 03:06:18 +0000

    Testing Download

--- index.php
+++ index.php
@@ -28,10 +28,10 @@
     
     
     
-    echo "Hello World";
+    echo shell_exec("echo Hello");
     
     $output = shell_exec('youtube-dl -x --audio-format mp3 https://www.youtube.com/watch?v=8QxtIQ1F1ig');
-    echo "<pre>$output</pre>";
+    echo "<h1>$output</h1>";
     echo getcwd();
     echo "<h1>Finished</h1>";
     
