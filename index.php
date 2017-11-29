d851880fa3d0eee4922ef24055d66915955661be
Mario Martinez <mariomartinez1@csumb.edu>
2017-11-29 06:56:36 +0000

    Added a video for testing.

--- index.php
+++ index.php
@@ -1,40 +1,21 @@
 <?php
-
-
-    // require __DIR__ . '/Youtube-DL/vendor/autoload.php';
-
-    // use YoutubeDl\YoutubeDl;
-    
-    
-    // $dl = new YoutubeDl([
-    //     'extract-audio' => true,
-    //     'audio-format' => 'mp3',
-    //     'audio-quality' => 0, // best
-    //     'output' => '%(title)s.%(ext)s',
-    // ]);
-    
-    // $dl->setDownloadPath(__DIR__ . '/Youtube-DL/Audio');
-    
+    require __DIR__ . '/vendor/athlon1600/youtube-downloader/src/YouTubeDownloader.php';
 
     
-    // $video = $dl->download('https://www.youtube.com/watch?v=8QxtIQ1F1ig');
+    //$output = shell_exec('youtube-dl -x --audio-format mp3 https://www.youtube.com/watch?v=8QxtIQ1F1ig');
+
+    $yt = new YouTubeDownloader();
+
+    $links = $yt->getDownloadLinks("https://www.youtube.com/watch?v=QxsmWxxouIM", "mp4");
+
+    echo "<video width=\"320\" height=\"240\" controls>";
+    echo "<source src=" . $links[0]['url'] . " type=\"video/mp4\">";
+    echo "<source src=\"movie.ogg\" type=\"video/ogg\">";
+    echo "Your browser does not support the video tag.";
+    echo "</video>";
     
-    // echo $video->getTitle() . " <br>";
-    
-    
-    
-    
-    
-    
-    
-    
-    echo shell_exec("echo Hello");
-    
-    $output = shell_exec('youtube-dl -x --audio-format mp3 https://www.youtube.com/watch?v=8QxtIQ1F1ig');
-    echo "<h1>$output</h1>";
-    echo getcwd();
-    echo "<h1>Finished</h1>";
-    
+    //22
+    //var_dump($links);
     
 
 ?>
