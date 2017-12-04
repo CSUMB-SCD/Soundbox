c2016b7244ae5fcb1be7348d5706310706d3604c
Mario Martinez <mariomartinez1@csumb.edu>
2017-12-04 10:51:50 +0000

    Changed from shell to system.

--- src/YoutubeAudioLink.php
+++ src/YoutubeAudioLink.php
@@ -42,7 +42,7 @@ class YoutubeAudioLink {
         $videoId = $this->mYoutube->parseVIdFromURL($yLink);
         $this->mVideoInfo = $this->mYoutube->getVideoInfo($videoId);
         
-        return shell_exec($this->mBinPath . $this->mAudioSettings . $yLink);
+        return system($this->mBinPath . $this->mAudioSettings . $yLink);
     }
     
     public function getYoutubeLinkFromSearchKeys($searchKey) {
