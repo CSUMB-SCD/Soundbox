feee30471302a18e042bbb76be65832ec2902ed5
DiegoaMedina <https://diegoamedina@github.com>
2017-12-15 05:01:44 +0000

    added the keyword test in the function name

--- tests/SpotifyTest.php
+++ tests/SpotifyTest.php
@@ -3,24 +3,24 @@
     use PHPUnit\Framework\TestCase;
     
     require_once __DIR__ . "/../src/Spotify.php";
-    
+
     final class SpotifyTest extends TestCase
     {
         
-        public function retrieveAccessToken() {
+        public function testRetrieveAccessToken() {
             $Spotify = new Spotify();
             $this->assertNull( $Spotify->retrieveAccessToken() );
+            $this->assertNull("hello");
         }
         
-        public function getClientId(){
+        public function testGetClientId(){
             $Spotify = new Spotify();
             $this->assertNotNull( $Spotify->getClientId() );
         }
         
-        public function getClientSecret(){
+        public function testGetClientSecret(){
             $Spotify = new Spotify();
             $this->assertNotNull( $Spotify->getClientSecret() );
         }
     }
     
-?>
